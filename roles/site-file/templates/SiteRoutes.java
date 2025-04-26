0ce75445e5ab0e3404ca105985feeae43a803945
Christopher Tate <computate@computate.org>
2025-04-25 23:17:44 -0600

    Latest fixes for kafka, and site user

--- roles/site-file/templates/SiteRoutes.java
+++ roles/site-file/templates/SiteRoutes.java
@@ -10,6 +10,9 @@ import {{ SITE_USER_PACKAGE }}.SiteUserEnUSApiServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import io.vertx.core.Future;
+import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
 import io.vertx.core.buffer.Buffer;
 import io.vertx.core.http.HttpResponseExpectation;
 import io.vertx.core.json.JsonObject;
@@ -17,10 +20,23 @@ import io.vertx.ext.web.Router;
 import io.vertx.ext.web.api.service.ServiceRequest;
 import io.vertx.ext.web.client.HttpRequest;
 import io.vertx.ext.web.client.WebClient;
+import io.vertx.kafka.client.consumer.KafkaConsumer;
 
 public class SiteRoutes {
 	protected static final Logger LOG = LoggerFactory.getLogger(SiteRoutes.class);
   
   public static void routes(Router router, ComputateOAuth2AuthHandlerImpl oauth2AuthHandler, JsonObject config, WebClient webClient, SiteUserEnUSApiServiceImpl apiSiteUser) {
+  }
+
+  public static Future<Void> kafkaConsumer(Vertx vertx, KafkaConsumer<String, String> consumer, JsonObject config) {
+    Promise<Void> promise = Promise.promise();
+    try {
+      promise.complete();
+    } catch(Exception ex) {
+      LOG.error("Unable to configure Kafka consumers. ", ex);
+      promise.fail(ex);
+    }
+
+    return promise.future();
   }
 }
--- roles/site-file/templates/SiteUser.java
+++ roles/site-file/templates/SiteUser.java
@@ -17,9 +17,6 @@ import {{ SITE_REQUEST_PACKAGE }}.SiteRequest;
  * Keyword: classSimpleNameSiteUser
  * Filter: userId
  * AuthUser: true
- * AuthDefaultClient:
- *   GET:
- *   PATCH:
  * 
  * SearchPageUri: /en-us/search/user
  * EditPageUri: /en-us/edit/user/{userId}
